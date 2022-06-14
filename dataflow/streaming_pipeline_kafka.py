92de06b6df086938aab3c4a086c32fac59df4edd
Sourabh Jain <jainsourabh2@users.noreply.github.com>
2022-06-14 11:56:53 +0530

    Create streaming_pipeline_kafka.py

---
+++ dataflow/streaming_pipeline_kafka.py
@@ -0,0 +1,88 @@
+#https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/kafkataxi/kafka_taxi.py
+#https://beam.apache.org/documentation/sdks/python-streaming/
+from apache_beam.options.pipeline_options import PipelineOptions
+from google.cloud import pubsub_v1
+from google.cloud import bigquery
+import apache_beam as beam
+import logging
+import argparse
+import sys
+import re
+from apache_beam.io.kafka import ReadFromKafka
+from apache_beam.io.kafka import WriteToKafka
+
+
+PROJECT="on-prem-project-337210"
+schema = 'remote_addr:STRING, timelocal:STRING, request_type:STRING, status:STRING, body_bytes_sent:STRING, http_referer:STRING, http_user_agent:STRING'
+TOPIC = "projects/on-prem-project-337210/topics/userlogs"
+bootstrap_servers = '123.45.67.89:123:9092'
+topic = 'kafka_taxirides_realtime'
+with_metadata = False
+
+def regex_clean(data):
+
+    PATTERNS =  [r'(^\S+\.[\S+\.]+\S+)\s',r'(?<=\[).+?(?=\])',
+           r'\"(\S+)\s(\S+)\s*(\S*)\"',r'\s(\d+)\s',r"(?<=\[).\d+(?=\])",
+           r'\"[A-Z][a-z]+', r'\"(http|https)://[a-z]+.[a-z]+.[a-z]+']
+    result = []
+    for match in PATTERNS:
+      try:
+        reg_match = re.search(match, data).group()
+        if reg_match:
+          result.append(reg_match)
+        else:
+          result.append(" ")
+      except:
+        print("There was an error with the regex search")
+    result = [x.strip() for x in result]
+    result = [x.replace('"', "") for x in result]
+    res = ','.join(result)
+    return res
+
+
+class Split(beam.DoFn):
+
+    def process(self, element):
+        from datetime import datetime
+        element = element.split(",")
+        d = datetime.strptime(element[1], "%d/%b/%Y:%H:%M:%S")
+        date_string = d.strftime("%Y-%m-%d %H:%M:%S")
+        
+        return [{ 
+            'remote_addr': element[0],
+            'timelocal': date_string,
+            'request_type': element[2],
+            'body_bytes_sent': element[3],
+            'status': element[4],
+            'http_referer': element[5],
+            'http_user_agent': element[6]
+    
+        }]
+
+def main(argv=None):
+
+   parser = argparse.ArgumentParser()
+   parser.add_argument("--input_topic")
+   parser.add_argument("--output")
+   known_args = parser.parse_known_args(argv)
+
+
+   p = beam.Pipeline(options=PipelineOptions())
+
+   (p
+      | 'ReadData' >> ReadFromKafka(
+            consumer_config={'bootstrap.servers': bootstrap_servers},
+            topics=[topic],
+            with_metadata=with_metadata)
+      | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
+      | "Clean Data" >> beam.Map(regex_clean)
+      | 'ParseCSV' >> beam.ParDo(Split())
+      | 'WriteToBigQuery' >> beam.io.WriteToBigQuery('{0}:userlogs.logdata'.format(PROJECT), schema=schema,
+        write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
+   )
+   result = p.run()
+   result.wait_until_finish()
+
+if __name__ == '__main__':
+  logger = logging.getLogger().setLevel(logging.INFO)
+  main()
