19871812aff78c7727cce8e95a4b83a595d50c52
Sourabh Jain <jainsourabh2@users.noreply.github.com>
2023-03-18 15:36:00 +0530

    Update app.js

--- cloudrun/hellow-world/app.js
+++ cloudrun/hellow-world/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', async (req, res) => {
-  res.send('Welcome to Hello World');
+  res.send('Welcome to Hello Universe');
 });
 
 const port = parseInt(process.env.PORT) || 3000;
